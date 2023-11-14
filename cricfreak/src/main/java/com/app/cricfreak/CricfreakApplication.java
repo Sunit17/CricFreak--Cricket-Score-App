ba61f1b66249682e70499ed79c3d216c7ae276c4
unknown <sunitkumarpattnaik22@gmail.com>
2023-11-14 13:26:18 +0530

    First commit with project structure

---
+++ cricfreak/src/main/java/com/app/cricfreak/CricfreakApplication.java
@@ -0,0 +1,13 @@
+package com.app.cricfreak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricfreakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricfreakApplication.class, args);
+	}
+
+}
